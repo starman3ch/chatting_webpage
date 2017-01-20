f068e1ee5809518aac15df56a1a37cf5d85d93db
lch131 <lch131@jiran.com>
2017-01-20 18:41:25 +0900

    client단 개발 진행 중..

--- routes/admin_login.js
+++ routes/admin_login.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) { // todo: app.js 에 있는거랑 맞춰줘야 되나?
-  res.render('admin_login', {});
+// router.get('/', function(req, res, next) { // todo: app.js 에 있는거랑 맞춰줘야 되나?
+//   res.render('admin_login', {});
+// });
+router.post('/', function(req, res){
+  console.log()
 });
 
 
